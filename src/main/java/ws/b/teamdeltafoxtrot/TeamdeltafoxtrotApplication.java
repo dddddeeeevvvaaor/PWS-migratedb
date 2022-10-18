6569f5ffc2c3750dc3b40f02c0dfca4dcc621399
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2022-10-18 14:26:49 +0700

    first commit

---
+++ src/main/java/ws/b/teamdeltafoxtrot/TeamdeltafoxtrotApplication.java
@@ -0,0 +1,13 @@
+package ws.b.teamdeltafoxtrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamdeltafoxtrotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamdeltafoxtrotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws/b/teamdeltafoxtrot/TeamdeltafoxtrotApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.b.teamdeltafoxtrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamdeltafoxtrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
